cf9c0e71b1125d14cf1c60236561393966c35e6b
ZoharKogan <zohar00kogan@gmail.com>
2026-08-03 19:34:42 +0300

    fix: remove .vercel folder from git to trigger real Vercel build

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-FxXv-Yok.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/zohar/Desktop/eco-watch-insight-main/eco-watch-insight-main/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-Dr14H12H.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-Dr14H12H.js" } }] }, "/": { filePath: "C:/Users/zohar/Desktop/eco-watch-insight-main/eco-watch-insight-main/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-DBhEGwNw.js"] } } });
-export {
-  tsrStartManifest
-};
